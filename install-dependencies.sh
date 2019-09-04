0c28c509c7776f7aec709e0e993702e5afa197b2
Akhlak Mahmood <amahmoo3@ncsu.edu>
2019-09-04 19:59:01 -0400

    add peewee to dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # installer
 
-pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib peewee
