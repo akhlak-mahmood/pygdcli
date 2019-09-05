4b7dde1388116841f560f4525815e68147ea00d9
Akhlak Mahmood <amahmoo3@ncsu.edu>
2019-09-05 00:42:08 -0400

    add errors.py

---
+++ gdclient/errors.py
@@ -0,0 +1,29 @@
+class ErrorNotFileSystemObject(TypeError):
+    pass
+
+class ErrorNotDriveFSObject(ErrorNotFileSystemObject):
+    pass 
+
+class ErrorNotLinuxFSObject(ErrorNotFileSystemObject):
+    pass
+
+class ErrorPathResolve(ValueError):
+    pass
+
+class ErrorTimeInvalid(ValueError):
+    pass
+
+class ErrorDirectionDetection(RuntimeError):
+    pass 
+
+class ErrorPathNotExists(FileExistsError):
+    pass 
+
+class ErrorParseResponseObject(RuntimeError):
+    pass 
+
+class ErrorIDNotSet(ValueError):
+    pass
+
+class ErrorNameNotSet(ValueError):
+    pass 
