6140d01c5b9e7fbbfd38c3aec1eb69e2276efdc4
Akhlak Mahmood <amahmoo3@ncsu.edu>
2019-09-20 23:41:02 -0400

    add dateutil and pytz to dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # installer
 
-pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib peewee
+pip install --upgrade pytz python-dateutil google-api-python-client google-auth-httplib2 google-auth-oauthlib peewee
